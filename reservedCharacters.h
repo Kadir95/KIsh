48a054a8a6678f35ac2b0c8f0de8840ab7f4af71
kadir95 <abdulkadirkundakcioglu@gmail.com>
2018-01-01 09:21:43 +0300

    parser exe.h

---
+++ reservedCharacters.h
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   reservedCharacters.h
+ * Author: mzp7
+ *
+ * Created on January 1, 2018, 2:30 AM
+ */
+
+#ifndef RESERVEDCHARACTERS_H
+#define RESERVEDCHARACTERS_H
+
+#define PIPE_CHARACTER  "|"
+#define APPEND_FILE     ">>"
+#define WRITE_FILE      ">"
+#define READ_FILE       "<"
+
+#define VARIABLE_TOKEN  "#"
+
+#endif /* RESERVEDCHARACTERS_H */
+
